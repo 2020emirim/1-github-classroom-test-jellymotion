4f6b32c40fd4423fc75e079a3e56137c8e78c621
jellymotion528 <jellymotion528@gmail.com>
2020-05-07 09:22:09 +0900

    polls/urls, views generic(Class-based view)

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.indexView, name='index'),
     # 127.0.0.1:8000/polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # 127.0.0.1:8000/polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView_as_view(), name='results'),
     # 127.0.0.1:8000/polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
